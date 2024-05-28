53ab7c425a1835b7758551833fefd526baaff7d5
Frnn4268 <appstutorialesy@gmail.com>
2024-05-28 15:51:33 -0600

    Adding new features

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,8 +1,8 @@
-export const host = "http://localhost:4000";
-export const loginRoute = `${process.env.REACT_APP_API_URL}/api/auth/login`;
-export const registerRoute = `${ process.env.REACT_APP_API_URL}/api/auth/register`;
-export const logoutRoute = `${ process.env.REACT_APP_API_URL}/api/auth/logout`;
-export const allUsersRoute = `${ process.env.REACT_APP_API_URL}/api/auth/allusers`;
-export const sendMessageRoute = `${ process.env.REACT_APP_API_URL}/api/messages/addmsg`;
-export const recieveMessageRoute = `${ process.env.REACT_APP_API_URL}/api/messages/getmsg`;
-export const setAvatarRoute = `${ process.env.REACT_APP_API_URL}/api/auth/setavatar`;
+export const host = process.env.REACT_APP_API_URL
+export const loginRoute = `${host}/api/auth/login`;
+export const registerRoute = `${host}/api/auth/register`;
+export const logoutRoute = `${host}/api/auth/logout`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const recieveMessageRoute = `${host}/api/messages/getmsg`;
+export const setAvatarRoute = `${host}/api/auth/setavatar`;
